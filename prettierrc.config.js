83bf5602e91c288fd0e3a632e09fc9aaa41470a0
Jared Boice2 <jaredboice2@Jareds-iMac.local>
2018-03-31 16:25:18 -0400

    updated prettier

---
+++ prettierrc.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 100,
+  parser: "babylon",
+  tabWidth: 4,
+  useTabs: true,
+  semi: true,
+  trailingComma: "none",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: "avoid",
+  plugins: ["jsx", "flow"]
+};
